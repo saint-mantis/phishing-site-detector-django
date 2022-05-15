d8b1dacce69277b18d0f754c982b6eed9c88a859
nodbox <arunbabu1021@gmail.com>
2022-05-15 20:13:20 +0530

    updates

--- phishing_site/main/views.py
+++ phishing_site/main/views.py
@@ -17,29 +17,35 @@ def CheckURL(request):
     end=[]
     urlArray=[]
     BASE_DIR = Path(__file__).resolve().parent.parent
+
     form = siteForm(request.POST)
     url = request.POST.get("url")
     print(url)
     urlArray.append(url)
+
     modelPath = os.path.join(BASE_DIR,'main/model.pkl')
     with open(modelPath, 'rb') as pred:
         lr = pickle.load(pred)
     prediction=lr.predict(urlArray) 
     print(prediction[0])
     prediction=prediction[0]
+
     if prediction =='good':
         TheSite.append('The Site')
         end.append('is not a Phishing Site')
+
     elif prediction == 'bad':
         TheSite.append('The Site')
         end.append('is a Phishing Site')
 
     else:
         TheSite.append("Error Occured")
+
     passPrediction= TheSite[0]
     form = siteForm()
     link= urlArray[0]
     end=end[0]
+    
     context={
     'prediction':passPrediction,
     'form':form,
@@ -48,5 +54,3 @@ def CheckURL(request):
     'end':end
     }
     return render(request, 'index.html',context)
-    #The Site {urlArray[0]} is not a Phishing Site'
-    #The Site {urlArray[0]} is a Phishing site'
